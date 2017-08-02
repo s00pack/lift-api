112692e9f9899b915687e874a7b550c756f49f6e
EXTENDA_NT\peack <petter.ackebo@extenda.se>
2017-08-02 08:14:33 +0200

    Added User and UserGroup

---
+++ LiftApi/Models/User.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LiftApi.Models
+{
+    /// <summary>
+    /// Addidtional information about a user. Connects to ApplicationUser
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// ID, do not set
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Identification / passsword class, Must be created before User
+        /// </summary>
+        public ApplicationUser ApplicationUser { get; set; }
+
+        /// <summary>
+        /// First name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Birth date, not mandatory
+        /// </summary>
+        public DateTime? BirthDate { get; set; }
+
+        /// <summary>
+        /// The users data and results are available to watch for all users
+        /// </summary>
+        public bool AllowPublicDisplay { get; set; }
+
+        /// <summary>
+        /// The users data and results are available to watch for all users in the groups the user is member of
+        /// </summary>
+        public bool AllowGroupDisplay { get; set; }
+
+        /// <summary>
+        /// The groups the user is member of
+        /// </summary>
+        public virtual ICollection<UserGroup> UserGroups { get; set; }
+
+    }
+}
---
+++ LiftApi/Models/UserGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LiftApi.Models
+{
+    public class UserGroup
+    {
+        public int UserGroupId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
