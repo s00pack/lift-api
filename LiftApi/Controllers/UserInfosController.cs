3258ee15b10515b818b6eb238db98a751beb1df9
EXTENDA_NT\peack <petter.ackebo@extenda.se>
2017-08-02 10:04:32 +0200

    Added logging

--- LiftApi/Controllers/UserInfosController.cs
+++ LiftApi/Controllers/UserInfosController.cs
@@ -9,96 +9,144 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using LiftApi.Models;
+using NLog;
 
 namespace LiftApi.Controllers
 {
     public class UserInfosController : ApiController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-
+        private Logger _logger = LogManager.GetCurrentClassLogger();
         // GET: api/UserInfos
         public IQueryable<UserInfo> GetUserInfos()
         {
-            return db.UserInfos;
+            _logger.Debug("Enter");
+            try
+            {
+                return db.UserInfos;
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e);
+                throw;
+            }
+
         }
 
         // GET: api/UserInfos/5
         [ResponseType(typeof(UserInfo))]
         public IHttpActionResult GetUserInfo(int id)
         {
-            UserInfo userInfo = db.UserInfos.Find(id);
-            if (userInfo == null)
+            _logger.Debug("Enter");
+            try
             {
-                return NotFound();
-            }
+                UserInfo userInfo = db.UserInfos.Find(id);
+                if (userInfo == null)
+                {
+                    return NotFound();
+                }
 
-            return Ok(userInfo);
+                return Ok(userInfo);
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e);
+                throw;
+            }
         }
 
         // PUT: api/UserInfos/5
         [ResponseType(typeof(void))]
         public IHttpActionResult PutUserInfo(int id, UserInfo userInfo)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != userInfo.UserInfoId)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(userInfo).State = EntityState.Modified;
-
+            _logger.Debug("Enter");
             try
             {
-                db.SaveChanges();
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                if (id != userInfo.UserInfoId)
+                {
+                    return BadRequest();
+                }
+
+                db.Entry(userInfo).State = EntityState.Modified;
+
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserInfoExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                return StatusCode(HttpStatusCode.NoContent);
             }
-            catch (DbUpdateConcurrencyException)
+            catch (Exception e)
             {
-                if (!UserInfoExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                _logger.Error(e);
+                throw;
             }
 
-            return StatusCode(HttpStatusCode.NoContent);
         }
 
         // POST: api/UserInfos
         [ResponseType(typeof(UserInfo))]
         public IHttpActionResult PostUserInfo(UserInfo userInfo)
         {
-            if (!ModelState.IsValid)
+            _logger.Debug("Enter");
+            try
             {
-                return BadRequest(ModelState);
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                db.UserInfos.Add(userInfo);
+                db.SaveChanges();
+
+                return CreatedAtRoute("DefaultApi", new {id = userInfo.UserInfoId}, userInfo);
             }
-
-            db.UserInfos.Add(userInfo);
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = userInfo.UserInfoId }, userInfo);
+            catch (Exception e)
+            {
+                _logger.Error(e);
+                throw;
+            }
         }
 
         // DELETE: api/UserInfos/5
         [ResponseType(typeof(UserInfo))]
         public IHttpActionResult DeleteUserInfo(int id)
         {
-            UserInfo userInfo = db.UserInfos.Find(id);
-            if (userInfo == null)
+            _logger.Debug("Enter");
+            try
             {
-                return NotFound();
+                UserInfo userInfo = db.UserInfos.Find(id);
+                if (userInfo == null)
+                {
+                    return NotFound();
+                }
+
+                db.UserInfos.Remove(userInfo);
+                db.SaveChanges();
+
+                return Ok(userInfo);
             }
-
-            db.UserInfos.Remove(userInfo);
-            db.SaveChanges();
-
-            return Ok(userInfo);
+            catch (Exception e)
+            {
+                _logger.Error(e);
+                throw;
+            }
         }
 
         protected override void Dispose(bool disposing)
